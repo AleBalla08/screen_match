0594082e84b756d12544771f1071381a40099e4a
Alexandre Ballardin <aleballa08@gmail.com>
2025-06-17 09:56:13 -0300

    criando a classe Movie

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Movie myMovie = new Movie();
+        myMovie.name = "Django: Unchained";
+        myMovie.releaseYear = 2013;
+        myMovie.planIncluded = true;
+        myMovie.inMinutesDuration = 160;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
---
+++ src/Movie.java
@@ -0,0 +1,8 @@
+public class Movie {
+    String name;
+    int releaseYear;
+    boolean planIncluded;
+    double avaliation;
+    int totalAvaliations;
+    int inMinutesDuration;
+}
